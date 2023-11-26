68f69ae87c627e7a47eac1a57c504e9f59c65720
mikeoborotov <mikeoborotov@gmail.com>
2023-11-27 01:15:31 +0300

    problem 29 solved

--- problem-29/problem_29.py
+++ problem-29/problem_29.py
@@ -1,43 +1,66 @@
 import numpy as np
 
+# calculate colored edges
+def colored_edges(genome):
+    # init 
+    result = []
 
+    # run through genome
+    for i in genome:
+        s = chromosome_to_cycle(i)
+        for j in range(len(s) // 2):
+            head = 1 + 2 * j # calculate head
+            tail = (2 + 2 * j) % len(s) # calculate tail
+            e = (s[head], s[tail])
+            result.append(e)
+
+    return result
+
+# two break sorting problem
 def two_break_sorting(P, Q):
-    red = colored_edges(Q)
+    # init
+    colored = colored_edges(Q)
     path = [P]
+
+    # sort
     while two_break_distance(P, Q) > 0:
-        cycles = colored_edges_cycles(colored_edges(P), red)
+        cycles = colored_edges_cycles(colored_edges(P), colored)
         for i in cycles:
             if len(i) >= 4:
                 P = two_break_on_genome(P, i[0], i[1], i[3], i[2])
                 path.append(P)
                 break
+
     return path
 
-
+# two break on genome
 def two_break_on_genome(genome, i, j, k, l):
-    g = colored_edges(genome)
-    g = two_break_on_genome_graph(g, i, j, k, l)
-    genome = graph_to_genome(g)
+    graph = colored_edges(genome)
+    graph = two_break_on_genome_graph(graph, i, j, k, l)
+    genome = graph_to_genome(graph)
     return genome
 
-
-def two_break_on_genome_graph(g, i, j, k, l):
+# two break on genome graph
+def two_break_on_genome_graph(graph, i, j, k, l):
     rem = ((i, j), (j, i), (k, l), (l, k))
-    bg = [t for t in g if t not in rem]
+    bg = [t for t in graph if t not in rem]
     bg.append((i, k))
     bg.append((j, l))
     return bg
 
-
+# calculate dist
 def two_break_distance(P, Q):
     blue = colored_edges(P)
     red = colored_edges(Q)
+
     size = len(blue) + len(red)
-    l = colored_edges_cycles(blue, red)
-    return size // 2 - len(l)
+
+    length = colored_edges_cycles(blue, red)
+
+    return size // 2 - len(length)
 
 
-def permutation_list_to_str(p):
+def permutation_list_to_string(p):
     ps = []
     for i in p:
         if i > 0:
@@ -49,7 +72,7 @@ def permutation_list_to_str(p):
     return '(' + ' '.join(ps) + ')'
 
 
-def permutation_str_to_list(str_p):
+def permutation_string_to_list(str_p):
     p = list(map(int, str_p.strip()[1:-1].split(' ')))
     return p
 
@@ -57,11 +80,11 @@ def permutation_str_to_list(str_p):
 def format_sequence(s):
     fs = []
     for i in s:
-        str_p = permutation_list_to_str(i)
+        str_p = permutation_list_to_string(i)
         fs.append(str_p)
     return fs
 
-
+# chromosome to cyc func
 def chromosome_to_cycle(p):
     nodes = []
     for i in p:
@@ -74,6 +97,7 @@ def chromosome_to_cycle(p):
     return nodes
 
 
+# cyc to chromosome func
 def cycle_to_chromosome(nodes):
     p = []
     for j in range(0, len(nodes) // 2):
@@ -88,53 +112,52 @@ def cycle_to_chromosome(nodes):
 def genome_str_to_list(genome):
     lp = []
     for p in genome.split('(')[1:]:
-        p = permutation_str_to_list('(' + p)
+        p = permutation_string_to_list('(' + p)
         lp.append(p)
     return lp
 
-
-def colored_edges(genome):
-    g = []
-    for p in genome:
-        s = chromosome_to_cycle(p)
-        for j in range(len(s) // 2):
-            head = 1 + 2 * j
-            tail = (2 + 2 * j) % len(s)
-            e = (s[head], s[tail])
-            g.append(e)
-    return g
-
-
+# convert graph to genome func
 def graph_to_genome(g):
+
     genome = []
     visited = []
+
     adj = np.zeros(len(g) * 2, dtype=np.int)
+
     for t in g:
         adj[t[0] - 1] = t[1] - 1
         adj[t[1] - 1] = t[0] - 1
 
     for t in g:
         orig = t[0]
+
         if orig in visited:
             continue
-        visited.append(orig)
+
+        visited.append(orig) # visit
+
         if (orig % 2 == 0):
             closing = orig - 1
         else:
             closing = orig + 1
+
         p = []
         i = 0
+
         while (True):
+
             if (orig % 2 == 0):
                 p.append(orig // 2)
             else:
                 p.append(-(orig + 1) // 2)
             dest = adj[orig - 1] + 1
             i = i + 1
+
             if (i > 100):
 
                 return
             visited.append(dest)
+
             if (dest == closing):
                 genome.append(p)
                 break
@@ -142,16 +165,24 @@ def graph_to_genome(g):
                 orig = dest - 1
             else:
                 orig = dest + 1
+
             assert orig > 0
+
             visited.append(orig)
+
     return genome
 
 
+# colored edges cycles for blue and red
 def colored_edges_cycles(blue, red):
+    # init
     cycles = []
     size = len(blue) + len(red)
+
     adj = np.zeros(shape=(size, 2), dtype=np.int)
     visiteded = np.zeros(shape=size, dtype=np.bool)
+
+    # run through blue and red
     for e in blue:
         adj[e[0] - 1, 0] = e[1] - 1
         adj[e[1] - 1, 0] = e[0] - 1
@@ -159,28 +190,37 @@ def colored_edges_cycles(blue, red):
         adj[e[0] - 1, 1] = e[1] - 1
         adj[e[1] - 1, 1] = e[0] - 1
 
+    # run through nodes
     for node in range(size):
         if not visiteded[node]:
-            visiteded[node] = True
-            head = node
+
+            visiteded[node] = True # visit
+            head = node # upd head
+
             cycle = [head + 1]
             color = 0
+
             while True:
                 node = adj[node, color]
+
                 if node == head:
                     cycles.append(cycle)
                     break
+
                 cycle.append(node + 1)
                 visiteded[node] = True
                 color = (color + 1) % 2
+
     return cycles
 
 
 if __name__ == '__main__':
-    with open('dataset.txt') as f:
-        P = [list(map(int, f.readline().strip()[1:-1].split(' ')))]
-        Q = [list(map(int, f.readline().strip()[1:-1].split(' ')))]
-    path = two_break_sorting(P, Q)
-    result = ''
-    for p in path:
-        print(''.join(format_sequence(p)))
+    # read and prepare dataset
+    with open('dataset.txt') as file:
+        P = [list(map(int, file.readline().strip()[1:-1].split(' ')))]
+        Q = [list(map(int, file.readline().strip()[1:-1].split(' ')))]
+    path = two_break_sorting(P, Q) # sort
+    
+    # print result
+    for i in path:
+        print(''.join(format_sequence(i)))
